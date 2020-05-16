08fea84fb990898f765b8eb7ff7464c2f927219f
Curt Bryan <curtbryan3rd@gmail.com>
2020-05-16 10:02:33 -0700

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-toolbar flat app class="grey lighten-3">
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-list dark flat tile="false">
+        <v-list-item-group v-for="link in links"  :key="link">
+          <v-list-item >
+            <v-list-item-icon>
+              <v-icon>{{link.icon}}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{link.text}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  Name: 'Navbar',
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Projects', route: '/projects' },
+        { icon: 'person', text: 'Team', route: '/team' },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
